2e728ffd3d46875a50d6f414412c2dd0ddbc4e46
iterathi <contact@iterathi.com>
2025-08-17 13:38:24 -0400

    changed theme settings

--- init.lua
+++ init.lua
@@ -44,7 +44,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 
 -- Custom vim/nvim/lazy/plugin settings
 vim.g.lazyvim_check_order = false -- disable lazy load order checking
-vim.g.nord_disable_background = true -- Disable Nord theme background to use terminal background (for transparency)
+-- vim.g.nord_disable_background = true -- Disable Nord theme background to use terminal background (for transparency)
 
 -- Set vim tab/spaces options
 vim.opt.tabstop = 2
@@ -225,13 +225,13 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
 
-  {
-    'shaunsingh/nord.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd 'colorscheme nord'
-    end,
-  },
+  --  {
+  --    'shaunsingh/nord.nvim',
+  --    priority = 1000,
+  --    config = function()
+  --      vim.cmd 'colorscheme nord'
+  --    end,
+  --  },
 
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
@@ -853,27 +853,28 @@ require('lazy').setup({
     },
   },
 
-  --{ -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --'folke/tokyonight.nvim',
-  --priority = 1000, -- Make sure to load this before all the other start plugins.
-  --config = function()
-  -----@diagnostic disable-next-line: missing-fields
-  --require('tokyonight').setup {
-  --styles = {
-  --comments = { italic = false }, -- Disable italics in comments
-  --},
-  --}
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        transparent = true, -- doesn't set the background color, used terminals background
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
 
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --vim.cmd.colorscheme 'tokyonight-night'
-  --end,
-  --},
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
